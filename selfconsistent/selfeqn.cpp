1009543aab5409a1c03dfd0ab31adcc3183cbf97
tobiom <22276261+tobiom@users.noreply.github.com>
2019-10-19 10:42:32 +0900

    self consistent equation

--- selfconsistent/selfeqn.cpp
+++ selfconsistent/selfeqn.cpp
@@ -4,7 +4,10 @@
 #include<cmath>
 #define _USE_MATH_DEFINES
 
-#define MAX(a,b) (((a) >= (b)) ? (a) : (b))
+#define MAX(a,b) (a>b? a:b)
+#define MIN(a,b) (a>b? b:a)
+#define rep(i,n) for(int i=0;i<(int)(n);i++)
+
 #define DIFF 0.00000001
 #define GRVTY 9.80665
 
@@ -13,6 +16,28 @@ using ll = long long;
 using ld = long double;
 using lint = long long int;
 
+//equation: x=f(x)
+inline ld func(ld x) {
+    return expl(-x*x);
+}
+
 int main(void){
+    // Steffensenの反復法
+    ld dx;
+    ld x[3]={};
+    ld ep=0.0001;
+    x[0] = 1.5;
+    rep(i,1000) {
+        cout << i << ":" << x[0] << endl;
+        x[1] = func(x[0]);
+        x[2] = func(x[1]);
+        dx = powl(x[1]-x[0],2.0)/(x[0]-2.0*x[1]+x[2]);
+        x[0] -= dx;
+        if(abs(dx)<ep) {
+            cout << "stop at i=" << i << endl;
+            break;
+        }
+    }
+    cout << x[0] << endl;
     return 0;
 }
---
+++ temp.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<fstream>
+#include<iomanip>
+#include<cmath>
+#define _USE_MATH_DEFINES
+
+#define MAX(a,b) (a>b? a:b)
+#define MIN(a,b) (a>b? b:a)
+#define rep(i,n) for(int i=0;i<(int)(n);i++)
+
+#define DIFF 0.00000001
+#define GRVTY 9.80665
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+using lint = long long int;
+
+int main(void){
+    return 0;
+}
